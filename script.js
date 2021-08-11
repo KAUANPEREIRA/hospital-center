a606476b9b89d4908dbafb52bfd3567c4ed5ef42
KAUANSANTOS <66709503+KAUANPEREIRA@users.noreply.github.com>
2021-08-10 21:53:19 -0300

    Menu hamburguer
    Atualizando a funcionalidade do menu para mobile utilizando html5, css, javascript.

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 function abrir() {
-    let menu=document.getElementsByClassName('menuMobile')
+
+  
+    let menu=document.querySelector('.menuMobile')
      let lista =document.getElementById('lista')
-    if (lista.style.display === "flex") {
+    if (lista.style.display !="flex") {
       lista.style.display = "none";
     } else {
       lista.style.display = "flex";
     }
   }
 
-function letra(){
-  let pai= document.getElementById
-}
